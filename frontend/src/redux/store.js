b0291d56cfd95adeed583794a826a27baa406e4b
DmiTsa <dmitrytaskaev.dt@gmail.com>
2024-04-25 19:28:08 +0300

    1

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import booksReducer from './books/reducer'
+import filterReducer from './slices/filterSlice'
 
 const store = configureStore(
     {
-        reducer: { books: booksReducer }
+        reducer: {
+            books: booksReducer,
+            filter: filterReducer
+        }
     }
 )
 
